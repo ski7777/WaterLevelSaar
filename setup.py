ccaf029bc200a0388e8080da967df638f967ee1a
Raphael Jacob <r.jacob2002@gmail.com>
2018-07-20 17:56:17 +0200

    Bum version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(name='WaterLevelSaar',
       description='Python Interface for water levels in Saarland/Germany',
-      version='0.1.0',
+      version='1.0.0',
       author='Raphael Jacob',
       author_email='r.jacob2002@gmail.com',
       url='https://github.com/ski7777/WaterLevelSaar',
